f605d8118580259758ccc9abff0ecae5728dc4ac
CalvoM <mwadimemakokha@ieee.org>
2020-05-05 00:05:21 +0300

    feat: Add connect method to mqtt server
    Sends Control packet to server

--- client.go
+++ client.go
@@ -1,6 +1,6 @@
 package mqtt
 
-//Client :
+//Client implements the mqtt client
 type Client struct{}
 
 //packet
@@ -20,4 +20,19 @@ type client interface {
 	Unsubscribe()
 	Ping()
 	Disconnect()
+}
+
+//Connect
+func (c *Client) Connect() {
+	pkt := packet{}
+	pkt.fixedHeader[0] = 0x10
+	//Protocol Name
+	pkt.variableHeader[0] = byte(0)
+	pkt.variableHeader[1] = byte(4)
+	pkt.variableHeader[2] = byte('M')
+	pkt.variableHeader[3] = byte('Q')
+	pkt.variableHeader[4] = byte('T')
+	pkt.variableHeader[5] = byte('T')
+	//Protocol Level
+	pkt.variableHeader[6] = byte(4)
 }
