46d3f01b77cab06984ce92a62eda85c472d007b0
CalvoM <mwadimemakokha@ieee.org>
2020-05-04 23:28:32 +0300

    Define client interface with control packets types as methods

--- client.go
+++ client.go
@@ -7,5 +7,17 @@ type Client struct{}
 type packet struct {
 	fixedHeader    []byte
 	variableHeader []byte
-	payLoad        []byte
+	payload        []byte
+}
+type client interface {
+	Connect()
+	Publish()
+	AcknowledgedPublish()
+	ReceivedPublish()
+	ReleasePublish()
+	CompletedPublish()
+	Subscribe()
+	Unsubscribe()
+	Ping()
+	Disconnect()
 }
